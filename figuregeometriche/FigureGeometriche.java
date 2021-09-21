5718b3ea629772de320f042d4094fa71fd19b888
Simone Vannucci <79282303+simonevann@users.noreply.github.com>
2021-09-21 12:39:35 +0200

    Update FigureGeometriche.java

--- figuregeometriche/FigureGeometriche.java
+++ figuregeometriche/FigureGeometriche.java
@@ -1,29 +1,44 @@
 package figuregeometriche;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Esercizio ereditarietà
  * @author Simone Vannucci
  */
 public class FigureGeometriche {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {       
+        Square sq = new Square(122,11);
+        System.out.println(sq.getSides());
+        try {
+            sq.setLengthSide(12.22);
+        } catch (InvalidNumberOfSideException ex) {
+            Logger.getLogger(FigureGeometriche.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println(sq.getPerimeter());
         
-      //Test
-      Square sq = new Square(122,11);
-      System.out.println(sq.getSides());
-      sq.setLengthSide(12);
-      System.out.println(sq.getPerimeter());
-
-      Triangle tr = new Triangle (113,42);
-      tr.setLengthSides(2, 0);
-      tr.setLengthSides(10, 1);
-      tr.setLengthSides(5, 2);
-       tr.setLengthSides(5, 3);
-      System.out.println(tr.getPerimeter());
-
-      TriangleEqui eq = new TriangleEqui(223,554);
-      eq.setLengthSide(10);
-      System.out.println(eq.getPerimeter());       
+        Triangle tr = new Triangle (113,42);
+        try {
+            tr.setLengthSides(2, 0);
+            tr.setLengthSides(10, 1);
+            tr.setLengthSides(5, 2);
+            tr.setLengthSides(5, 3);
+        } catch (InvalidNumberOfSideException ex) {
+            Logger.getLogger(FigureGeometriche.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println(tr.getPerimeter());
+        
+        TriangleEqui eq = new TriangleEqui(223,554);
+        try {
+            eq.setLengthSide(10);
+        } catch (InvalidNumberOfSideException ex) {
+            Logger.getLogger(FigureGeometriche.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println(eq.getPerimeter());
+        
     }
     
 }
