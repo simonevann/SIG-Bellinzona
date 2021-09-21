d327cfcd5c29b8d68d2af7052b8a42be1abe89c0
Simone Vannucci <79282303+simonevann@users.noreply.github.com>
2021-09-21 11:37:57 +0200

    Upload code

---
+++ figuregeometriche/Square.java
@@ -0,0 +1,33 @@
+
+package figuregeometriche;
+
+/**
+ * Definition of a square
+ * @author Simone Vannucci
+ */
+public class Square extends Polygon{
+    
+    public static int NSIDES = 4; //Number of side in  square
+    public static boolean EQUILATERAL = true; //Is equilateral
+    
+    /**
+     * Set the square sides length
+     * @param length of the side
+     */
+    public void setLengthSide(int length){
+        super.setLengthSides(length, 0);
+    }
+    
+    /**
+     * Get the perimeter ot the square
+     * @return perimeter
+     */
+    @Override
+    public int getPerimeter(){
+        return super.getLengthSide(0) * Square.NSIDES;
+    }
+    
+    public Square(int x, int y){
+        super(x, y, Square.NSIDES, Square.EQUILATERAL);
+    }
+}
