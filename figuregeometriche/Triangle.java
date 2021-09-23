357b38a85a5a2a7b23ddd6a5f1439c6ce7e6cb72
Simone Vannucci <79282303+simonevann@users.noreply.github.com>
2021-09-23 10:06:16 +0200

    Update Triangle.java

--- figuregeometriche/Triangle.java
+++ figuregeometriche/Triangle.java
@@ -1,44 +1,59 @@
-
 package figuregeometriche;
 
 /**
- * Definition of a triangle
+ * Definition of an equilateral triangle
  * @author Simone Vannucci
  */
-public class Triangle extends Polygon{
-
-    public static int NSIDES = 3; //Number of side in  triangle
+public class TriangleEqui extends Triangle{
     
-    /**
-     * Crating a triangle
-     * @param x position
-     * @param y position
-     * @throws InvalidNumberOfSideException 
+    public static boolean EQUILATERAL = true; //Is equilateral
+    
+      /**
+     * Set the equilateral triangle sides length
+     * @param length of the side
      */
-    public Triangle (int x, int y) throws InvalidNumberOfSideException{
-        super(x, y, Triangle.NSIDES);
+    public void setLengthSide(double length) throws InvalidNumberOfSideException{
+        super.setLengthSides(length, 0);
     }
     
     /**
-     * Crating a triangle
-     * @param x position
-     * @param y position
-     * @param equilateral is equilateral
+     * Overriding the setLengthSides in Polygon class to mantain all sides equal
+     * @param length
+     * @param i
      * @throws InvalidNumberOfSideException 
      */
-    public Triangle (int x, int y, boolean equilateral) throws InvalidNumberOfSideException{
-        super(x, y, Triangle.NSIDES, equilateral);
+    @Override
+    public void setLengthSides(double length, int i) throws InvalidNumberOfSideException {
+        this.setLengthSide(length);
     }
     
-     /**
-     * Crating a triangle
+      /**
+     * Get the perimeter ot the equilateral triangle
+     * @return perimeter
+     */
+    @Override
+    public double getPerimeter(){
+        return super.getLengthSide(0) * Triangle.NSIDES;
+    }
+    
+    /**
+     * Crating an equilateral triangle
      * @param x position
      * @param y position
-     * @param sideLengths array with the lenghts of the sides
      * @throws InvalidNumberOfSideException 
      */
-    public Triangle (int x, int y, double sidesideLenghtA, double sidesideLenghtB, double sidesideLenghtC) throws InvalidNumberOfSideException{      
-        super(x, y, new double[] { sidesideLenghtA, sidesideLenghtB, sidesideLenghtC });
+    public TriangleEqui (int x, int y) throws InvalidNumberOfSideException{
+        super(x, y, true);
     }
     
+    /**
+     * Crating an equilateral triangle
+     * @param x position
+     * @param y position
+     * @param length lenght of the sides
+     * @throws InvalidNumberOfSideException 
+     */
+    public TriangleEqui (int x, int y, double sideLenght) throws InvalidNumberOfSideException{
+        super( x,  y,  sideLenght, sideLenght, sideLenght);
+    } 
 }
